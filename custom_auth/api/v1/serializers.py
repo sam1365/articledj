d2042263fb927bff1c1c59af7b314fe1212e7a30
aisam <moradi.sayyedali@gmail.com>
2025-09-12 18:43:27 +0330

    feat: Added github_username To Retuned Object Of get_token Methon In CustomTokenObtainPairSerializer

--- custom_auth/api/v1/serializers.py
+++ custom_auth/api/v1/serializers.py
@@ -6,5 +6,6 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
+        token['github_username'] = user.github_username
         token['email'] = user.email
         return token
